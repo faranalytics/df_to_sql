44feaca9687208891c7f5fffe3ca5ba71fa69ea8
adamjpatterson <adam@farar.net>
2024-11-02 10:34:14 -0400

    Rename class.

--- src/df_to_sql/__init__.py
+++ src/df_to_sql/__init__.py
@@ -1,3 +1,3 @@
-from .main import DFToSQL
+from .data_frame_to_sql import DataFrameToSQL
 date_regexes: list[str] = [r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}([+-]\d{2}:\d{2}|Z)?$']
 number_regexes: list[str] = [r'^([1-9][0-9]+|[0-9])(\.[0-9]+$|$)']
